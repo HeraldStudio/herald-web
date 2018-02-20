14c9c7ec72e06862b54b36979f986ae0febb7615
rikumi <riku.mi@outlook.com>
2018-02-21 00:56:36 +0800

    屏保带时间啦

--- src/pages/ScreenSaver.vue
+++ src/pages/ScreenSaver.vue
@@ -3,7 +3,9 @@
   #live2d-page
     .live2d-container
       live2d(:follow-cursor='false', :auto-emotion='true')
-   
+    .clock-container(:class='{ shakeup: shakeup }')
+      .clock {{ time }}
+
 </template>
 <script>
 
@@ -12,9 +14,27 @@
   export default {
     components: {
       live2d
+    },
+    data () {
+      return {
+        time: '',
+        shakeup: false
+      }
+    },
+    created () {
+      setInterval(() => {
+        let now = new Date()
+        let hour = now.getHours()
+        let min = now.getMinutes()
+        if (min < 10) {
+          min = '0' + min
+        }
+        this.time = hour + ':' + min
+        this.shakeup = !this.shakeup
+      }, 500)
     }
   }
-  
+
 </script>
 <style lang="stylus" scoped>
 
@@ -27,7 +47,7 @@
     z-index 9999
     background #000
     display flex
-    flex-direction column
+    flex-direction row
     align-items center
     justify-content center
 
@@ -35,5 +55,38 @@
       width 200px
       height 200px
       position relative
+
+    .clock-container
+      border-radius 60px
+      border 2px solid #fff
+      position relative
+      margin-bottom 90px
+      transform rotate(-15deg)
+      transform-origin 0 50%
+      transition .5s
+
+      &.shakeup
+        transform rotate(-25deg)
+
+      &::before
+        content ''
+        position absolute
+        left -10px
+        top 50%
+        margin-top -7px
+        border-top 7px solid transparent
+        border-bottom 7px solid transparent
+        border-right 10px solid #fff
+
+      .clock
+        background #fff
+        font-size 40px
+        font-family 'SFNS Display', 'Helvetica Neue', 'Agency FB'
+        font-weight 500
+        line-height 1em
+        color #5d5d5d
+        border-radius 60px
+        border 8px solid #21a9f0
+        padding 7px 15px
 
 </style>
