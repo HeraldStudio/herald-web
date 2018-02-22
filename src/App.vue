fd2891ac588742112e157d4a53774b1603d5d8d4
rikumi <riku.mi@outlook.com>
2018-02-22 21:00:35 +0800

    先去掉PWA

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,7 @@
       if (window.navigator.standalone) {
         this.webapp = true
       }
-      offline.install()
+      // offline.install()
       logger.bindAjax()
 
       let checkLogin = async () => {
