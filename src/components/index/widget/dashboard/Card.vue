c25ab837f17e395e27e37480eb32c29931187fd9
rikumi <riku.mi@outlook.com>
2018-03-16 13:18:56 +0800

    充值提示

--- src/components/index/widget/dashboard/Card.vue
+++ src/components/index/widget/dashboard/Card.vue
@@ -13,6 +13,10 @@
           drawer(title='一卡通充值')
             .title 充值
             .content(slot='content')
+              p.hint.
+                一卡通在线预充值并非实时到账，在校内多数食堂超市网点刷卡时均可到账；
+                极少数情况不能到账时，你也可以手动在圈存机存入任意金额；
+                本功能仅在银行卡与学校一卡通中心之间产生交易，与小猴偷米无关。
               .amount-select
                 .amount(v-for='amount in [50, 100, 200, 300, 500]' :class='{ selected: charge.amount === amount }' @click='charge.amount = amount') {{ amount }}
               input(placeholder='金额' v-model='charge.amount')
@@ -156,5 +160,23 @@
 
       input
         text-align center
+
+      .hint
+        display block
+        font-size 14px
+        margin-bottom 15px
+        white-space normal
+        color var(--color-text-secondary)
+
+        &::before
+          content '!'
+          display inline-block
+          width 20px
+          height 20px
+          margin-right 5px
+          border-radius 50%
+          background var(--color-warning-light)
+          color var(--color-warning-dark)
+          text-align center
 
 </style>
