deb6f2d5a6d0dc501a329e26bfae882eef21f3f4
rikumi <riku.mi@outlook.com>
2018-05-16 09:25:28 +0800

    课表预测登录

--- src/components/CourseStat.vue
+++ src/components/CourseStat.vue
@@ -1,38 +1,40 @@
 <template lang="pug">
 
-  #course-stat(v-if='user')
-    .title 小猴偷米课表预测 v3.0 build 369e263f
-    .hint(v-if='stat.length')
-      p.
-        此列表根据你所在院系最近一段时间成绩查询记录的匿名统计，根据教务处排课编号排除你已经上过的课程，
-        从而推测出你下学期（短学期和长学期）可能上到的必选、限选、任选课程，供选课时参考。
-    .list(v-if='stat.length')
-      .course(v-for='(course, i) in stat' :key='course.cid')
-        .head(v-if='i == 0 || course.semester != stat[i - 1].semester') 第{{ course.semester }}学期
-        .line
-          .name {{ course.courseName }}
-          .credit {{ course.credit }} 学分
-        .line
-          .info(v-if='course.avgScore') 样本容量 {{ course.sampleCount }} / 粗略平均成绩 {{ course.avgScore }}
-          .info(v-else) 样本容量 {{ course.sampleCount }}
-    .hint(v-if='stat.length')
-      p.
-        1. 由于隐私保护以及性能要求，调查并未覆盖全部学生，且存在可控范围内的重复计算。
-        样本容量为未去重值，仅作为选课人数比例参考，不代表实际人数。
-      p.
-        2. 粗略平均成绩仅供参考，按等级计分的课程，以教务处绩点算法下等价的百分制分数换算。
-    .hint(v-else) 找不到与你的院系年级对应的数据
-    hr
-    .title 热门选修课排行
-    .list(v-if='optional.length')
-      .course(v-for='course in optional' :key='course.cid')
-        .line
-          .name {{ course.courseName }}
-          .credit {{ course.courseType.split('').slice(0, 2).join('') }}
-        .line
-          .info(v-if='course.avgScore') 样本容量 {{ course.sampleCount }} / 粗略平均成绩 {{ course.avgScore }}
-          .info(v-else) 样本容量 {{ course.sampleCount }}
-      .load-more(v-if='!ended' @click='loadOptional()') ↓ 加载更多
+  #course-stat
+    .course-stat(v-if='user')
+      .title 小猴偷米课表预测 v3.0 build 369e263f
+      .hint(v-if='stat.length')
+        p.
+          此列表根据你所在院系最近一段时间成绩查询记录的匿名统计，根据教务处排课编号排除你已经上过的课程，
+          从而推测出你下学期（短学期和长学期）可能上到的必选、限选、任选课程，供选课时参考。
+      .list(v-if='stat.length')
+        .course(v-for='(course, i) in stat' :key='course.cid')
+          .head(v-if='i == 0 || course.semester != stat[i - 1].semester') 第{{ course.semester }}学期
+          .line
+            .name {{ course.courseName }}
+            .credit {{ course.credit }} 学分
+          .line
+            .info(v-if='course.avgScore') 样本容量 {{ course.sampleCount }} / 粗略平均成绩 {{ course.avgScore }}
+            .info(v-else) 样本容量 {{ course.sampleCount }}
+      .hint(v-if='stat.length')
+        p.
+          1. 由于隐私保护以及性能要求，调查并未覆盖全部学生，且存在可控范围内的重复计算。
+          样本容量为未去重值，仅作为选课人数比例参考，不代表实际人数。
+        p.
+          2. 粗略平均成绩仅供参考，按等级计分的课程，以教务处绩点算法下等价的百分制分数换算。
+      .hint(v-else) 找不到与你的院系年级对应的数据
+      hr
+      .title 热门选修课排行
+      .list(v-if='optional.length')
+        .course(v-for='course in optional' :key='course.cid')
+          .line
+            .name {{ course.courseName }}
+            .credit {{ course.courseType.split('').slice(0, 2).join('') }}
+          .line
+            .info(v-if='course.avgScore') 样本容量 {{ course.sampleCount }} / 粗略平均成绩 {{ course.avgScore }}
+            .info(v-else) 样本容量 {{ course.sampleCount }}
+        .load-more(v-if='!ended' @click='loadOptional()') ↓ 加载更多
+    login(v-else)
 
 </template>
 <script>
