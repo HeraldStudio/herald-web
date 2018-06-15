80e39587e7d06b9d420fe81e841291792d2a2c3b
rikumi <riku.mi@outlook.com>
2018-06-15 11:15:13 +0800

    修正课表点击学期时学期变为空的问题

--- src/components/Curriculum.vue
+++ src/components/Curriculum.vue
@@ -4,7 +4,7 @@
     .week-picker
       .switch(@click='listView = !listView; displayTerm = ""') {{ listView ? '列表视图' : '周视图' }}
       .prev(v-if='!listView' @click='prevTerm()') ‹
-      button.cur(v-if='!listView' title='点击回到本学期' @click='displayTerm = ""') {{ displayTerm }}
+      button.cur(v-if='!listView' title='点击回到本学期' @click='displayTerm = currentTerm') {{ displayTerm }}
       .next(v-if='!listView' @click='nextTerm()') ›
       .prev(v-if='!listView' @click='prevWeek()') ‹
       button.cur(v-if='!listView' title='点击回到本周' @click='displayWeek = currentWeek') {{ displayWeek }} 周
