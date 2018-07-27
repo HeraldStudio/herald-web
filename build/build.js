2dd6ae53a65e9e8b734f8c383f6b6a3ad76643e2
rikumi <riku.mi@outlook.com>
2018-07-27 23:15:26 +0800

    小调整

--- build/build.js
+++ build/build.js
@@ -19,7 +19,7 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   webpack(webpackConfig, function (err, stats) {
     spinner.stop()
     if (err) throw err
-    process.stdout.write(stats.toString({
+    console.log(stats.toString({
       colors: true,
       modules: false,
       children: false,
