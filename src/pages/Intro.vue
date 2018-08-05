8acac5dd484648b546c24747293852ba8d1175f7
rikumi <riku.mi@outlook.com>
2018-08-05 18:39:45 +0800

    调整

--- src/pages/Intro.vue
+++ src/pages/Intro.vue
@@ -19,7 +19,7 @@
             {{ user.schoolnum.substr(3, 2) }} 年级 /
             {{ user.schoolnum.substr(5, 1) }} 班 /
             {{ user.schoolnum.substr(6, 2) }} 号
-          .left(v-if='!user.schoolnum') 待分配班级学号中，请留意院系通知
+          .left(v-if='!user.schoolnum') 待分配班级学号中，请留意院系新生群通知
       li
         .top
           .left 统一身份认证密码
@@ -30,8 +30,10 @@
         .top
           .left 宿舍
           .right(v-if='dorm && dorm.building') {{ dorm.campus }} {{ dorm.building }} {{ dorm.room }} {{ dorm.bed }}号床
+          .right(v-else) 暂未分配宿舍
         .bottom
-          .left 可到<a href='http://map.seu.edu.cn/#/home'>校园地图服务系统</a>查看宿舍楼所在位置。
+          .left(v-if='dorm && dorm.building') 可到<a href='http://map.seu.edu.cn/#/home'>校园地图服务系统</a>查看宿舍楼所在位置。
+          .left(v-else) 待分配宿舍中，请留意班级群通知
 
 </template>
 <script>
