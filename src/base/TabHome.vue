bdbe0821fcd1d5a0a6a1083b03fda9cc98d2ce8a
Wolf-Tungsten <canis.tungsten@foxmail.com>
2018-10-27 19:41:21 +0800

    增加课表页，主页添加小猴提醒

--- src/base/TabHome.vue
+++ src/base/TabHome.vue
@@ -3,6 +3,7 @@
     dashboard(v-if='user' :user='user')
     login(v-else)
     .todo(v-if='user')
+      toutiao
       exam(v-if='/^21/.test(user.cardnum)')
       curriculum
       experiment(v-if='/^21/.test(user.cardnum)')
@@ -14,12 +15,13 @@
   import dashboard from '@/components/Dashboard.vue'
   import curriculum from '@/components/Curriculum.vue'
   import experiment from '@/components/Experiment.vue'
+  import toutiao from '@/components/TouTiao.vue'
   import api from '@/api'
 
   export default {
     props: ['user'],
     components: {
-      login, dashboard, curriculum, experiment, exam
+      login, dashboard, curriculum, experiment, exam, toutiao
     }
   }
 </script>
---
+++ src/components/TouTiao.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+
+  .widget.toutiao
+    span.title 小猴提醒
+    span.notice(@click='showNotice') {{notice}}
+
+</template>
+<script>
+import api from "@/api";
+import formatter from "@/util/formatter";
+
+export default {
+  data() {
+    return {
+      notice: "",
+      nid: 0
+    };
+  },
+  created() {
+    api.get("/api/notice").then(notice => {
+      notice.forEach(k => {
+        if (k.site === "小猴偷米") {
+          this.notice = k.title;
+          this.nid = k.nid;
+        }
+      });
+    });
+  },
+  methods: {
+      showNotice(){
+          this.$router.push('/notice/' + this.nid)
+      }
+  }
+};
+</script>
+<style lang="stylus" scoped>
+.toutiao {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    .title {
+        text-align: left;
+        font-size: 15px;
+        font-weight: bold;
+        color: var(--color-text-bold);
+        padding-right: 10px;
+        border-right: 0.5px solid var(--color-divider)
+    }
+
+    .notice {
+        margin-left 10px;
+        color: var(--color-primary);
+    }
+}
+</style>
