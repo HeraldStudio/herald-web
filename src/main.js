7bf8f475025207182342db5c81cc21e8fd569673
rikumi <riku.mi@outlook.com>
2019-01-20 00:05:57 +0800

    修正问题

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import injectManifest from './manifest'
 
 import Toasted from 'vue-toasted'
 import Persist from 'vue-component-persist'
