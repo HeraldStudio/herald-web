5335f301d8ec4faf74a6a9a5938c3b8830328423
HHHKKKHHH <1790733272@qq.com>
2020-08-09 11:04:26 +0800

    bugfix

--- src/pages/Download.vue
+++ src/pages/Download.vue
@@ -11,57 +11,62 @@
         
 </template>
 <script>
-  import qrcodeImg from 'static/images/qrcode.jpg'
-
-  export default {
-    data() {
-      return { qrcodeImg,url:"" }
-    },
-    async created() {
-      console.log("get version")
-      let res =  await api.get("/api/version");
-      this.url = res.data.downloadUrl;
-    }
+import qrcodeImg from "static/images/qrcode.jpg";
+import api from "@/api";
+export default {
+  data() {
+    return { qrcodeImg, url: "" };
+  },
+  async created() {
+    let res = await api.get("/api/version");
+    this.url = res.data.downloadUrl;
   }
+};
 </script>
 <style lang='stylus' scoped>
+.page {
+  text-align: center;
 
-  .page
-    text-align center
+  .hint {
+    display: block;
+    font-size: 14px;
+    margin-bottom: 15px;
 
-    .hint
-      display block
-      font-size 14px
-      margin-bottom 15px
+    &::before {
+      content: '!';
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      margin-right: 5px;
+      border-radius: 50%;
+      background: var(--color-warning-light);
+      color: var(--color-warning-dark);
+      text-align: center;
+    }
+  }
 
-      &::before
-        content '!'
-        display inline-block
-        width 20px
-        height 20px
-        margin-right 5px
-        border-radius 50%
-        background var(--color-warning-light)
-        color var(--color-warning-dark)
-        text-align center
+  .buttons {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 25px;
+    align-items: center;
 
-    .buttons
-      display flex
-      flex-wrap wrap
-      justify-content center
-      margin-top 25px
-      align-items center
+    button {
+      margin: 5px 0;
+    }
 
-      button
-        margin 5px 0
+    * + * {
+      margin-left: 10px;
+    }
+  }
 
-      * + *
-        margin-left 10px
-
-    img.qr
-      width 100%
-      height auto
-      filter hue-rotate(-15deg)
-      max-width 500px
-      margin 0 auto
+  img.qr {
+    width: 100%;
+    height: auto;
+    filter: hue-rotate(-15deg);
+    max-width: 500px;
+    margin: 0 auto;
+  }
+}
 </style>
