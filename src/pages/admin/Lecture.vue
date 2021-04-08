c1b2c0de2e0e8bcbb24a7fe4e44470292bb8d6d2
Wave-SYJ <sun1yu1jia1@outlook.com>
2021-04-09 00:30:44 +0800

    添加删除讲座

--- src/pages/admin/Lecture.vue
+++ src/pages/admin/Lecture.vue
@@ -8,6 +8,25 @@
         th.dateStr 日期
         th.url 网址
         th.operations 操作
+      tr.lecture.add
+        td
+          input.name(placeholder="新增", v-model="newLecture.name")
+        td
+          input.location(v-model="newLecture.location")
+        td
+          timestamp.dateStr(
+            v-model="newLecture.date",
+            useType="date",
+            showType="date"
+          )
+        td
+          input.url(v-model="newLecture.url")
+        td.operations
+          confirm-button(
+            v-if="newLecture.name && newLecture.location && newLecture.date",
+            @click="addLecture()" confirm-text='确定新增'
+          ) 新增讲座
+
       tr.lecture(v-for="(lecture, index) in lectures")
         td
           input.name(v-model="lecture.name")
@@ -22,9 +41,14 @@
         td
           input.url(v-model="lecture.url")
         td.operations
-          button.save(v-if="lecture.name") 保存
-          confirm-button.remove 删除
-    page-bar(:current='pagination.current' :pageSize='pagination.pageSize' :total='pagination.total' @change='changePage')
+          button.save(v-if="lecture.name") 上传数据
+          confirm-button.remove(@click='removeLecture(lecture.id)' confirm-text='确定') 删除
+    page-bar(
+      :current="pagination.current",
+      :pageSize="pagination.pageSize",
+      :total="pagination.total",
+      @change="changePage"
+    )
 </template>
 
 <script>
@@ -52,50 +76,59 @@ export default {
       }
     };
   },
-  async created() {
-    this.originLectures = (await api.get("/api/lectureManage")).map(item => ({
-      ...item,
-      date: new Date(item.dateStr).getTime()
-    }));
-    this.initNewLecture();
-    this.changePage({
-      ...this.pagination,
-      total: this.originLectures.length
-    });
+  created() {
+    this.reloadData();
   },
   methods: {
+    async reloadData() {
+      this.originLectures = (await api.get("/api/lectureManage")).map(item => ({
+        ...item,
+        date: new Date(item.dateStr).getTime()
+      })).sort((item1, item2) => item2.date - item1.date);
+      this.initNewLecture();
+      this.changePage({
+        ...this.pagination,
+        total: this.originLectures.length
+      });
+    },
     initNewLecture() {
-      const now = new Date();
       this.newBanner = {
         id: null,
         name: "",
-        dateStr: `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`,
+        date: null,
         url: "",
         location: ""
       };
     },
     changePage(pagination) {
       this.pagination = pagination;
-      console.log(pagination);
       this.lectures = this.originLectures.slice(
         (pagination.current - 1) * pagination.pageSize + 1,
         (pagination.current - 1) * pagination.pageSize + 1 + pagination.pageSize
       );
-      console.log(
-        (pagination.current - 1) * pagination.pageSize + 1,
-        (pagination.current - 1) * pagination.pageSize + 1 + pagination.pageSize
-      );
+    },
+    async addLecture() {
+      const date = new Date(this.newLecture.date)
+      this.newLecture.dateStr = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+      await api.post('/api/lectureManage', this.newLecture)
+      this.reloadData()
+    },
+    async removeLecture(id) {
+      await api.delete('/api/lectureManage?id=' + id)
+      this.reloadData()
     }
   }
 };
 </script>
 
 <style lang="stylus">
+.list {
+  .location {
+    width: 150px;
+  }
 
-.list
-  .lecture-header
-    .location
-      width 150px
-    .dateStr
-      width 115px
+  .dateStr {
+    width: 115px;
+  }
+}
 </style>
